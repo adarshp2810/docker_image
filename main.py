8ec9892693c3b9a3937d710d987312dfcf49b35d
Fabi <93148668+Fabi18@users.noreply.github.com>
2025-06-18 13:09:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2040,10 +2040,13 @@ class RiskDataModel:
             elif group_by_field in self.df_customer.columns:
                 all_vals = self.df_customer[group_by_field].dropna().unique()
                 full_index = pd.DataFrame({group_by_field: all_vals})
-            else:
-                series = self.df_joined[group_by_field].dropna()
+            elif group_by_field == "staging":
+                series = self.df_joined[staging].dropna()
                 cleaned_series = pd.Series(series).apply(lambda x: str(x).strip() if isinstance(x, str) else str(x))
                 all_vals = natsorted(cleaned_series.unique())
+                full_index = pd.DataFrame({group_by_field: all_vals})
+            else:
+                all_vals = df[group_by_field].dropna().unique()
                 full_index = pd.DataFrame({group_by_field: all_vals})
 
             # Merge full index with result
